7039bb691d5bc63a1b279199cad777698207e632
Camila <camiart@gmail.com>
2023-08-13 14:35:00 -0300

    exercicio04

---
+++ src/components/button/button.jsx
@@ -0,0 +1,11 @@
+import { ButtonStyled } from "./styled";
+function Button() {
+    return (
+        <ButtonStyled>
+            Texto do Botão
+        </ButtonStyled>
+      
+    );
+  }
+  
+  export default Button;
---
+++ src/components/button/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ButtonStyled = styled.button`
+  background-color: #007bff;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+`;
